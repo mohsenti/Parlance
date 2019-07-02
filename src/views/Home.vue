c5faf77049814bbff949632dd92bef81e4e9806f
Mohsen <mohsen.timar@gmail.com>
2019-07-03 03:34:41 +0430

    Make Websocket connection with server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="home">
-    
-  </div>
+  <div class="home"></div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-
+import { Component, Vue } from "vue-property-decorator";
+import SocketIO from "socket.io-client";
 @Component
 export default class Home extends Vue {
-  
+  private io: SocketIOClient.Socket;
+  constructor() {
+    super();
+    this.io = SocketIO("http://localhost:3001");
+    this.io.on("connect", (socket: any) => {
+      console.log("Connected");
+      this.io.emit("message", "HelloWorld");
+    });
+    this.io.on("message", (msg: any) => {
+      console.log(msg);
+    });
+    this.io.on("disconnect", () => {
+      console.log("DisConnected");
+    });
+  }
 }
 </script>
