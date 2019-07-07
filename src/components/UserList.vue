e948d868fa18b6ab423c7e444ca93a10673f7cf3
Mohsen <mohsen.timar@gmail.com>
2019-07-07 14:02:40 +0430

    UserList UI

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-    <div class="search-panel d-flex flex-column align-items-center p-2 border-bottom">
+    <div class="search-panel d-flex flex-column align-items-center p-2">
       <b-input type="text" placeholder="Search" />
     </div>
+    <User v-for="user in users" v-bind:key="user.uniqueName" :info="user" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-@Component
-export default class UserList extends Vue {}
+import User from "@/components/User.vue";
+import { UserModel } from "@/models";
+
+@Component({
+  components: {
+    User
+  }
+})
+export default class UserList extends Vue {
+  public users: UserModel[] = [];
+  mounted() {
+    for (let i = 1; i < 10; i++) {
+      this.users.push({
+        name: i.toString(),
+        uniqueName: i.toString(),
+        avatarURL: i.toString(),
+        lastMessage: i.toString(),
+        lastMessageTime: i.toString(),
+        budget: i
+      });
+    }
+  }
+}
 </script>
 
 
